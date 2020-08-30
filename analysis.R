7e62d16c4f1a94143a221df6891008a501afef13
Shiori Kanno <shiori.kanno@my.jcu.edu.au>
2020-08-31 09:53:55 +1000

    Bug fixed and summary added

--- analysis.R
+++ analysis.R
@@ -1,5 +1,7 @@
 # 31-Aug-2020 Murry Logan'S R course Day 1
 # Introduction to "Git"
-x = seq(1, 10, len = 1)
+# .gitignore ignores files that you don't need
+x = seq(1, 10, len = 10)
 y = 40*2 + rnorm(10,0,5)
 plot (x,y)
+summary(x)
